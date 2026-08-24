f95ff0938dbb6824e555407125999871ebc5afac
phuc <106951568+phucdevvn@users.noreply.github.com>
2026-08-24 18:56:52 +0700

    Add app entry point

---
+++ Sources/CardVoice/CardVoiceApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct CardVoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.titleBar)
+        .defaultSize(width: 1120, height: 780)
+    }
+}
